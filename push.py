e572d2ee927f61fe85bf46762afe3944f9940685
Aneesh KS <aneeshkaarthiks@gmail.com>
2019-10-05 10:03:28 +0000

    push button + LED

--- push.py
+++ push.py
@@ -1,8 +1,10 @@
 import gpiozero
 button = gpiozero.Button(17)
-
+led = gpiozero.LED(4)
 while True:
       if button.is_pressed:
-        print("button is pressed")
+        print("button pressed")
+        led.on()
       else:
-          print("button is not pressed")
+        led.off()
+        print("button not pressed")
