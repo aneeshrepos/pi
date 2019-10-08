9fd88048c0c6378111b6e97162864d831b96c5f7
Aneesh KS <aneeshkaarthiks@gmail.com>
2019-10-08 09:18:19 +0000

    delayed off of light

--- push.py
+++ push.py
@@ -1,10 +1,12 @@
 import gpiozero
+import time
 button = gpiozero.Button(17)
 led = gpiozero.LED(4)
 while True:
       if button.is_pressed:
         print("button pressed")
         led.on()
+        time.sleep(1)
       else:
         led.off()
         print("button not pressed")
